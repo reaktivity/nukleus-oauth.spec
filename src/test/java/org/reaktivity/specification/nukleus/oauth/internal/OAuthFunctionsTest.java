1098b2ac4b244ea5776eb1f8a5da992117214e7f
dknoma <djknoma@gmail.com>
2019-07-15 15:00:39 -0700

    added another test to confirm that specifying issuer and audiences on a route will fail authorizing a token if they do not include the correct issuer or audience. This is the opposite of the previous test where not specifying issuer or audience should authorize a token with the correct realm & scope if they include/don't include any issuer or audience

--- src/test/java/org/reaktivity/specification/nukleus/oauth/internal/OAuthFunctionsTest.java
+++ src/test/java/org/reaktivity/specification/nukleus/oauth/internal/OAuthFunctionsTest.java
@@ -82,14 +82,14 @@ public final class OAuthFunctionsTest
     public void shouldFailBadRIntegrity()
     {
         OAuthFunctions.decodeIntegrity(new byte[]{
-                48, 69, 2, 33,
-                // r
-                1, -7, -33, 20, -11, -42, -105, -25, -46, -85, 76, -111, -100, -66, -80, 93, -83, -113, -40, -19, -41, -90, 108,
-                -72, -71, -40, -112, -54, 113, 33, -84, -87, -103,
-                2, 32,
-                // s
-                15, -114, -34, 122, -28, -122, 41, -47, 37, 32, 15, -32, -2, -111, -30, 20, -69, 41, 1, -84, -59, 7, -102, -39,
-                -97, 63, -7, -60, -18, 0, -4, -30
+            48, 69, 2, 33,
+            // r
+            1, -7, -33, 20, -11, -42, -105, -25, -46, -85, 76, -111, -100, -66, -80, 93, -83, -113, -40, -19, -41, -90, 108,
+            -72, -71, -40, -112, -54, 113, 33, -84, -87, -103,
+            2, 32,
+            // s
+            15, -114, -34, 122, -28, -122, 41, -47, 37, 32, 15, -32, -2, -111, -30, 20, -69, 41, 1, -84, -59, 7, -102, -39,
+            -97, 63, -7, -60, -18, 0, -4, -30
 
         });
     }
@@ -98,14 +98,14 @@ public final class OAuthFunctionsTest
     public void shouldFailBadSIntegrity()
     {
         OAuthFunctions.decodeIntegrity(new byte[]{
-                48, 69, 2, 32,
-                // r
-                127, -112, 89, -73, -40, -1, -33, -91, -127, -70, -87, -38, 61, 93, -30, -97, 49, 49, 90, 100, -1, -21, -37, -63,
-                -24, -63, 22, 71, -17, -23, 49, -47,
-                2, 33,
-                // s
-                127, -65, -40, -51, 77, -7, 90, -73, -34, -68, 75, -115, -117, 19, -13, 86, -42, -67, -41, 79, -117, 2, -14, 126,
-                -66, 50, 121, 28, 79, 110, 8, 50, -36
+            48, 69, 2, 32,
+            // r
+            127, -112, 89, -73, -40, -1, -33, -91, -127, -70, -87, -38, 61, 93, -30, -97, 49, 49, 90, 100, -1, -21, -37, -63,
+            -24, -63, 22, 71, -17, -23, 49, -47,
+            2, 33,
+            // s
+            127, -65, -40, -51, 77, -7, 90, -73, -34, -68, 75, -115, -117, 19, -13, 86, -42, -67, -41, 79, -117, 2, -14, 126,
+            -66, 50, 121, 28, 79, 110, 8, 50, -36
         });
     }
 
@@ -120,12 +120,12 @@ public final class OAuthFunctionsTest
     {
         byte[] build = OAuthFunctions.resolveEx()
                                      .issuer("test issuer")
-                                     .audience("test audience")
+                                     .audience("testAudience")
                                      .build();
         DirectBuffer buffer = new UnsafeBuffer(build);
         OAuthResolveExFW resolveEx = new OAuthResolveExFW().wrap(buffer, 0, buffer.capacity());
 
         assertEquals("test issuer", resolveEx.issuer().asString());
-        assertEquals("test audience", resolveEx.audience().asString());
+        assertEquals("testAudience", resolveEx.audience().asString());
     }
 }
